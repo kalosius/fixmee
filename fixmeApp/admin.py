9bfa92d30d540f6bfe1fe277d832d2cf1bdb7572
kalosius <kalosius43@gmail.com>
2024-12-16 17:56:24 +0300

    car brand model created

--- fixmeApp/admin.py
+++ fixmeApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . models import Car, CarBrand
 
 # Register your models here.
+@admin.register(CarBrand)
+class CarBrand(admin.ModelAdmin):
+    ordering = ['brand_name'] 
+
+admin.site.register(Car)
