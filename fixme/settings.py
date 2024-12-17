448113fffc350fbe8641a641a9bf96cfa312cd6b
kalosius <kalosius43@gmail.com>
2024-12-17 19:54:58 +0300

    settings

--- fixme/settings.py
+++ fixme/settings.py
@@ -110,6 +110,7 @@ STATIC_URL = 'static/'
 #     os.path.join(BASE_DIR, "static/"),
 # ]
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
