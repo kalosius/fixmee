8b3b6ebae1b6ac1894032db6b079da2002ee5ef2
kalosius <kalosius43@gmail.com>
2025-01-09 10:57:57 +0300

    side fixed

--- fixmeApp/static/js/script.js
+++ fixmeApp/static/js/script.js
@@ -1,16 +1,8 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     const navbarToggler = document.querySelector('.navbar-toggler');
+//     const navbarCollapse = document.querySelector('#navbarSupportedContent');
 
-// let sidenav = document.querySelector('#side');
-
-// function smallScreen(){
-//     sidenav.style.display = 'block';
-// }
-
-// function bigScreen(){
-//     sidenav.style.display = 'none';
-// }
-
-// if (window.innerWidth <= 768){
-//     smallScreen();
-// }else{
-//     bigScreen;
-// }
+//     navbarToggler.addEventListener('click', function() {
+//         navbarCollapse.classList.toggle('show');
+//     });
+// });
