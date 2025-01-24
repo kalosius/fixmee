0d86e49b2c4c380cfe610ee576f05d775e2dbd31
kalosius <kalosius43@gmail.com>
2025-01-24 17:53:55 +0300

    refresh project

--- fixme/settings.py
+++ fixme/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import os
-import environ
+import environ 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -13,7 +13,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q9sgzzp5@cs1aaujc)f!%paf%^su@t9r@07-5b-ipdte0@tbcb'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
