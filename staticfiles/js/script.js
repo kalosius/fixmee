13eafabb5e2c8aec86daf1b55c62b1deb3dae2d9
kalosius <kalosius43@gmail.com>
2025-02-04 14:43:26 +0300

    side nav

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,8 +1,23 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     const navbarToggler = document.querySelector('.navbar-toggler');
-//     const navbarCollapse = document.querySelector('#navbarSupportedContent');
+let sidenave = document.querySelector('.sidenav');
+let sidedisplay = document.querySelector('#sidedisplay');
+let sideclose = document.createElement('button');
+sideclose.textContent = 'X';
+sideclose.style.position = 'absolute';
+sideclose.style.top = '10px';
+sideclose.style.right = '10px';
+sideclose.style.padding = '30px';
+sideclose.style.background = 'transparent';
+sideclose.style.border = 'none';
+sideclose.style.fontSize = '20px';
+sideclose.style.cursor = 'pointer';
+sideclose.style.color = '#fff';
 
-//     navbarToggler.addEventListener('click', function() {
-//         navbarCollapse.classList.toggle('show');
-//     });
-// });
+sidenave.appendChild(sideclose);
+
+sidedisplay.addEventListener('click', function() {
+    sidenave.style.display = 'block';
+});
+
+sideclose.addEventListener('click', function() {
+    sidenave.style.display = 'none';
+});
