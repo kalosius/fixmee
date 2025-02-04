ad6a06de09e85eefe8862663af3bde45d7b4709e
Aloisius Kasozi <124684740+kalosius@users.noreply.github.com>
2025-02-04 16:30:36 +0300

    Update settings.py

--- fixme/settings.py
+++ fixme/settings.py
@@ -16,7 +16,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://fixmee.onrender.com', 'fixmee.onrender.com', 'localhost', '127.0.0.1']
 
