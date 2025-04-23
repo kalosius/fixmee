ef169e22ec8fcfa7f09b21caa1fa8e938edca423
kalosius <kalosius43@gmail.com>
2025-04-23 14:54:31 +0300

    Testing Production Error

--- fixme/settings.py
+++ fixme/settings.py
@@ -15,7 +15,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://fixme-coc6.onrender.com', 'fixme-coc6.onrender.com' 'https://fixmee.onrender.com', 'fixmee.onrender.com', 'localhost', '127.0.0.1']
 
