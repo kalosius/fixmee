0bb82df1ce817398aaaa8bb87fe8330541e11a03
kalosius <kalosius43@gmail.com>
2025-04-23 14:50:30 +0300

    bad request

--- fixme/settings.py
+++ fixme/settings.py
@@ -4,7 +4,6 @@ import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 # Initialize environment variables
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
@@ -49,7 +48,7 @@ ROOT_URLCONF = 'fixme.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
